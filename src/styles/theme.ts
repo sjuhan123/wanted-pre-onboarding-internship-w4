68dfe9800bc3e067fdc06705af74f48a903772c3
sjuhan123 <sjuhan123@gmail.com>
2023-09-11 15:55:10 +0900

    ✨ feat: GlobalStyle, theme, font 설정

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  COLORS: {
+    TURQUISE: "rgb(36, 224, 188)",
+    ROYAL_BULE: "rgb(67, 139, 255)",
+    DEEP_BLUE: "rgb(44, 91, 242)",
+    LIGHT_BLUE: "rgb(0, 188, 212)",
+    LIGHT_RED: "rgb(255, 82, 82)",
+    LIGHT_GRAY: "rgb(224, 224, 224)",
+  },
+};
+
+export { theme };
