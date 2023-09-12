05e0f5af2a282ed1ee74d37fc8d24f7233992de9
sjuhan123 <sjuhan123@gmail.com>
2023-09-12 17:11:55 +0900

    🔧 chore: jest.config.ts setupFilesAfterEnv path 수

--- jest.config.ts
+++ jest.config.ts
@@ -9,5 +9,5 @@ export default {
     "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/src/test/__mocks__/fileMock.js",
     "\\.(css|less|scss)$": "identity-obj-proxy",
   },
-  setupFilesAfterEnv: ["<rootDir>/src/test/jest.setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
