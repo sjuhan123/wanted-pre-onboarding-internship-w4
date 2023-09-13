90ccf9d67a0a3d8febeb13ecbfa02cfd938c0eb6
sjuhan123 <sjuhan123@gmail.com>
2023-09-13 19:55:19 +0900

    🔧 chore: BASE_URL 설정, .env 파일 추가, url 수정

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = "http://localhost:5173";
+export const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const API_PATH = Object.freeze({
-  REGION: "/api/region",
+  REGION: "region",
 });
