184d94e7114214593f6eb3b7ea4e282f02fcc832
sjuhan123 <sjuhan123@gmail.com>
2023-09-13 15:47:18 +0900

    ✨ feat: useFilter 반환 함수에 useCallback 추가

--- src/hooks/useFilter.tsx
+++ src/hooks/useFilter.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export const useFilter = () => {
   const [currentOption, setcurrentOption] = useState(0);
 
-  const handleFilter = (index: number) => {
-    if (index === currentOption) return;
-    setcurrentOption(index);
-  };
+  const handleFilter = useCallback(
+    (index: number) => {
+      if (index === currentOption) return;
+      setcurrentOption(index);
+    },
+    [currentOption]
+  );
 
   return { currentOption, handleFilter };
 };
